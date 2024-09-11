8810a0a4407f0419e588242b685786715b04c567
2021121 <2021121@B104-14>
2024-09-11 09:38:08 +0200

    Add project files.

---
+++ ahan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ahan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xd");
+            Console.ReadLine();
+        }
+    }
+}
